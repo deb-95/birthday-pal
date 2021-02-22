d5d900f9c8b18fa960e44ecfda6fe5f0d40934f7
Debora Del Vecchio <d.delvecchio@kevopartners.com>
2021-02-22 15:23:27 +0100

    added pedantic and easy_localization

--- lib/app/birthday_pal.dart
+++ lib/app/birthday_pal.dart
@@ -1,17 +1,28 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class BirthdayPal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BirthdayPal',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+    return EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('it')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: Builder(
+        builder: (localizationContext) => MaterialApp(
+          localizationsDelegates: localizationContext.localizationDelegates,
+          supportedLocales: localizationContext.supportedLocales,
+          locale: localizationContext.locale,
+          title: 'BirthdayPal',
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('Material App Bar'),
+            ),
+            body: Center(
+              child: Container(
+                child: Text('Hello World'),
+              ),
+            ),
           ),
         ),
       ),
